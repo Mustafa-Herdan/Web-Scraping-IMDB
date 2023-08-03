0c5f9c6abfd59c7608ac8fc30ecf359339e1f31f
Mustafa Saleh <136553800+Mustafa-Herdan@users.noreply.github.com>
2023-08-03 07:09:17 +0300

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -6,13 +6,14 @@ url = "https://www.imdb.com/search/title/?at=0&genres=animation&keywords=anime&n
 page = requests.get(url)
 soup = BeautifulSoup(page.text)
 
-df = pd.DataFrame(columns=["Rank", "Name", "Date", "Genre", "Rate"])
+df = pd.DataFrame(columns=["Rank", "Name", "Date", "Genre", "Rate", "Vote"])
 
 rank = soup.find_all("span", class_="lister-item-index unbold text-primary")
 name = soup.find_all("h3", class_="lister-item-header")
 date = soup.find_all("span", class_="lister-item-year text-muted unbold")
 genre = soup.find_all("span", class_="genre")
 rate = soup.find_all("strong")
+vote = soup.find_all("p", class_="sort-num_votes-visible")
 
 for i in range(len(rank)):
     ranks = rank[i].text.replace(".", "").strip()
@@ -20,7 +21,8 @@ for i in range(len(rank)):
     dates = date[i].text.strip()
     genres = genre[i].text.strip()
     rates = rate[i + 2].text.strip()
-    rows = [ranks, names, dates, genres, rates]
+    votes = vote[i].text[8:].strip()
+    rows = [ranks, names, dates, genres, rates, votes]
     df.loc[len(df)] = rows
 
 df.to_csv("Anime Imdb.csv", index=False)
